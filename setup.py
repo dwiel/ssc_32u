a2af27fe42ee2c77103d14fd0dcd7a191537e316
Zach Dwiel <zdwiel@plotwatt.com>
2017-03-09 21:08:45 -0800

    version 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ssc_32u',
-    version='0.1',
+    version='0.2',
     description='simple interface to robot arms '
                 'controlled with SSC-32U controller',
     url='http://github.com/dwiel/ssc_32u',
